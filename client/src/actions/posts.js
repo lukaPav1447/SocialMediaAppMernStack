29e4508d622e7a0a571f672bcda184cf76358337
Luka Pavlovic <luka.pavlovic.1447@gmail.com>
2023-02-13 20:08:56 +0100

    Creating a post req and creating post in mongodb

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,12 +1,21 @@
-import * as api from '../api';
+import * as api from "../api";
 
 export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+
+    dispatch({ type: "FETCH_ALL", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
     try {
-        const { data } = await api.fetchPosts();
+        const { data } = await api.createPost(post);
 
-        dispatch({type: 'FETCH_ALL', payload: data});
+        dispatch({ type: 'CREATE', payload: data});
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
-
 }
